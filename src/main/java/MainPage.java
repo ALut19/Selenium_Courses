0e3774761abcb116a4c343bf558b43c449b96a0f
ALut19 <andreylut19@gmail.com>
2018-01-18 21:05:54 +0300

    18.01.18 Добавлено задание 2

--- src/main/java/MainPage.java
+++ src/main/java/MainPage.java
@@ -1,5 +1,4 @@
 import org.junit.Assert;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
